b038f7d46a906c6dabdaf9e6a2f7698bbc2d6578
NickolaevA.E <aen15@tpu.ru>
2024-09-11 14:02:17 +0300

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import ProductList from "./components/productList";
 import ProductDetail from "./components/ProductDetail";
 import Header from "./components/Header";
@@ -14,7 +14,7 @@ const App = () => {
   };
 
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Header
         showFavorites={showFavorites}
         toggleShowFavorites={toggleShowFavorites}
